92433a907babf100f5c94d0b1343524fe168d30f
ThulanKula <61007442+ThulanKula@users.noreply.github.com>
2020-08-30 20:31:51 +0200

    Update StudentRepositoryImplTest.java

--- src/test/java/repository/impl/StudentRepositoryImplTest.java
+++ src/test/java/repository/impl/StudentRepositoryImplTest.java
@@ -6,16 +6,20 @@ import static org.junit.Assert.*;
 
 public class StudentRepositoryImplTest {
 
-    @Test
-    public void getStudentRepository() {
-    }
+   private static StudentRepository studentRepository = StudentRepositoryImpl.getStudentRepository();
+    private static Student student = StudentFactory.createStudent("217026666", "Thulani", "Kula");
 
     @Test
     public void getAll() {
+        System.out.println("Get Student: " + studentRepository.getAll());
     }
+
 
     @Test
     public void create() {
+        Student s = studentRepository.create(student);
+        Assert.assertEquals(s.getStudNumber(), s.getFirstName(), s.getLastName());
+        System.out.println(s);
     }
 
     @Test
