c02de24b3c7a36c74e361cfbc22608c452a99402
ThulanKula <thulanikula3@gmail.com>
2020-08-30 19:55:55 +0200

    2. Repository interface and package added

--- src/test/java/repository/impl/StudentRepositoryImplTest.java
+++ src/test/java/repository/impl/StudentRepositoryImplTest.java
@@ -1,21 +1,31 @@
 package repository.impl;
 
+import entity.Student;
+import entity.Year;
+import factory.StudentFactory;
+import factory.YearFactory;
+import org.junit.Assert;
 import org.junit.Test;
+import repository.StudentRepository;
+import repository.YearRepository;
 
 import static org.junit.Assert.*;
 
 public class StudentRepositoryImplTest {
 
-    @Test
-    public void getStudentRepository() {
-    }
+    private static StudentRepository studentRepository = StudentRepositoryImpl.getStudentRepository();
+    private static Student student = StudentFactory.createStudent("217026666", "Thulani", "Kula");
 
     @Test
     public void getAll() {
+        System.out.println("Get Student: " + studentRepository.getAll());
     }
 
     @Test
     public void create() {
+        Student s = studentRepository.create(student);
+        Assert.assertEquals(s.getStudNumber(), s.getFirstName(), s.getLastName());
+        System.out.println(s);
     }
 
     @Test
