e83655a7efdb3829361379870e5c326e4c3fa0c0
ThulanKula <thulanikula3@gmail.com>
2020-09-02 17:30:20 +0200

    1. Service interface and package added

--- src/main/java/service/IService.java
+++ src/main/java/service/IService.java
@@ -1,5 +1,11 @@
 package service;
 
+/**
+ * @Auhtor : Thulani Kula
+ * Date: 02 Sept. 2020
+ *
+ * **/
+
 public interface IService <Type, ID> {
     Type create (Type type);
     Type read (ID id);
