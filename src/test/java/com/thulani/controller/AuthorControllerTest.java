796972a22172efdd27e45c6ee1c59f872a5172ec
216286182 <60961323+216286182@users.noreply.github.com>
2020-09-27 21:31:19 +0200

    Update AuthorControllerTest

--- src/test/java/com/thulani/controller/AuthorControllerTest.java
+++ src/test/java/com/thulani/controller/AuthorControllerTest.java
@@ -2,6 +2,7 @@ package com.thulani.controller;
 
 import com.thulani.entity.Author;
 import com.thulani.factory.AuthorFactory;
+import junit.framework.TestCase;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,9 +10,11 @@ import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.HttpClientErrorException;
 
+import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @SpringBootTest(classes = Author.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -32,26 +35,46 @@ public class AuthorControllerTest {
         ResponseEntity<Author> postResponse = testRestTemplate.postForEntity(getURL(), author, Author.class);
         assertNotNull(postResponse);
         assertNotNull(postResponse.getBody());
-
-        System.out.println(postResponse);
-        System.out.println(postResponse.getBody());
-
     }
 
     @Test
-    public void b_testRead() {
+    public void b_findById() {
+        Author searchFor = testRestTemplate.getForObject(getURL() + "/find/"+"1010", Author.class);
+
+        assertNotNull(searchFor);
     }
 
     @Test
     public void c_testUpdate() {
+        Author update = testRestTemplate.getForObject(getURL()+"/find/" + "1010", Author.class);
+        Author.Builder update2 = new Author.Builder().setAuthFirstName("John").setAuthLastName("Doe");
+        testRestTemplate.put(getURL() + "/update/"+"1010",update2);
+        Author updated = testRestTemplate.getForObject(getURL() + "/update/"+"1010",Author.class);
+
+        assertNotNull(updated);
     }
 
     @Test
     public void d_testGetAll() {
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+        ResponseEntity<String> response = testRestTemplate.exchange(getURL() + "/getAll", HttpMethod.GET, entity, String.class);
+        assertNotNull(response.getBody());
     }
 
     @Test
     public void f_testDelete() {
 
+
+        try {
+            Author author = testRestTemplate.getForObject(getURL() + "/find/" + "1010", Author.class);
+            TestCase.assertNotNull(author);
+
+            testRestTemplate.delete(getURL() + "/delete/" + "1010");
+            author = testRestTemplate.getForObject(getURL() + "/find/" + "1010", Author.class);
+            System.out.println(author);
+        } catch (final HttpClientErrorException e) {
+            assertEquals(e.getStatusCode(), HttpStatus.NOT_FOUND);
+        }
     }
 }
