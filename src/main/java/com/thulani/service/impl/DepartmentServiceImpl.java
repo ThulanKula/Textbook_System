e2c43e91bd95615b10507ac7da550d4144ce8f7f
Nacisha Jaftha <60962427+Nacisha07@users.noreply.github.com>
2020-09-27 18:07:52 +0200

    DepartmentController added

--- src/main/java/com/thulani/service/impl/DepartmentServiceImpl.java
+++ src/main/java/com/thulani/service/impl/DepartmentServiceImpl.java
@@ -5,42 +5,51 @@ import com.thulani.repository.DepartmentRepository;
 import com.thulani.repository.impl.DepartmentRepositoryImpl;
 import com.thulani.service.DepartmentService;
 import java.util.Set;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DepartmentServiceImpl implements DepartmentService {
-    public static DepartmentService service = null;
-    private DepartmentRepository repository;
 
-    private DepartmentServiceImpl(){
-        this.repository = DepartmentRepositoryImpl.getDepartmentRepository();
+    public static DepartmentService departmentService = null;
+    private final DepartmentRepository departmentRepository;
+
+    private DepartmentServiceImpl() {
+        this.departmentRepository = DepartmentRepositoryImpl.getDepartmentRepository();
     }
 
-    public static DepartmentService getService(){
-        if (service == null) service = new DepartmentServiceImpl();
-        return service;
-    }
 
+    public static DepartmentService getDepartmentService() {
+        if (departmentService == null) {
+            departmentService = new DepartmentServiceImpl();
+        }
+        return departmentService;
+    }
     @Override
     public Set<Department> getAll() {
-        return this.repository.getAll();
+        return this.departmentRepository.getAll();
     }
 
     @Override
     public Department create(Department department) {
-        return this.repository.create(department);
+
+        return this.departmentRepository.create(department);
     }
 
     @Override
     public Department read(String s) {
-        return this.repository.read(s);
+
+        return this.departmentRepository.read(s);
     }
 
     @Override
     public Department update(Department department) {
-        return this.repository.update(department);
+
+        return this.departmentRepository.update(department);
     }
 
     @Override
     public boolean delete(String s) {
-        return this.repository.delete(s);
+
+        return this.departmentRepository.delete(s);
     }
 }
