de2aae2e12ed85b3068efa49a2f5c763cdaf1458
216286182 <60961323+216286182@users.noreply.github.com>
2020-11-08 23:03:40 +0200

    AuthorControllerTest Security

--- src/test/java/com/thulani/controller/AuthorControllerTest.java
+++ src/test/java/com/thulani/controller/AuthorControllerTest.java
@@ -23,11 +23,11 @@ import static org.junit.Assert.assertNotNull;
 public class AuthorControllerTest {
 
     private static Author author = AuthorFactory.createAuthor("Louise", "Smith");
-    private static String SECURITY_ADMIN = "staff";
-    private static String SECURITY_ADMIN_PASSWORD = "2000217";
+    private static String ADMIN_ROLE = "staff";
+    private static String USER_ROLE = "student";
+    private  static String ADMIN_PASSWORD = "2000217";
+    private  static String USER_PASSWORD = "2171000";
 
-    private static String SECURITY_USER = "student";
-    private  static String SECURITY_USER_PASSWORD = "2171000";
 
     @Autowired
     private TestRestTemplate testRestTemplate;
@@ -40,7 +40,7 @@ public class AuthorControllerTest {
         System.out.println("URL: "+ url);
         System.out.println("Post data: " + author);
         ResponseEntity<Author> postResponse = testRestTemplate
-                .withBasicAuth(SECURITY_USER, SECURITY_USER_PASSWORD)
+                .withBasicAuth(USER_ROLE, USER_PASSWORD)
                 .postForEntity(url, author, Author.class);
         assertNotNull(postResponse);
         assertNotNull(postResponse.getBody());
@@ -54,7 +54,8 @@ public class AuthorControllerTest {
         String url = baseUrl+ "read/"+author.getAuthNumber();
         System.out.println("URL: "+url);
         ResponseEntity<Author> responseEntity = testRestTemplate
-                .withBasicAuth(SECURITY_USER, SECURITY_USER_PASSWORD)
+                .withBasicAuth(ADMIN_ROLE, ADMIN_PASSWORD)
+                .withBasicAuth(USER_ROLE, USER_PASSWORD)
                 .getForEntity(url, Author.class);
         assertEquals(author.getAuthNumber(), responseEntity.getBody().getAuthNumber());
     }
@@ -67,7 +68,7 @@ public class AuthorControllerTest {
         System.out.println("URL: "+url);
         System.out.println("Post data: "+update);
         ResponseEntity<Author> responseEntity = testRestTemplate
-                .withBasicAuth(SECURITY_USER, SECURITY_USER_PASSWORD)
+                .withBasicAuth(USER_ROLE, USER_PASSWORD)
                 .postForEntity(url,update, Author.class);
         assertEquals(author.getAuthNumber(), responseEntity.getBody().getAuthNumber());
     }
@@ -79,19 +80,17 @@ public class AuthorControllerTest {
         HttpHeaders httpHeaders = new HttpHeaders();
         HttpEntity<String> entity = new HttpEntity<>(null, httpHeaders);
         ResponseEntity<String> response = testRestTemplate
-                .withBasicAuth(SECURITY_USER, SECURITY_USER_PASSWORD)
                 .exchange(url, HttpMethod.GET, entity, String.class);
         System.out.println(response);
         System.out.println(response.getBody());
     }
 
     @Test
-    @Ignore
     public void e_delete() {
         String url = baseUrl+"delete/"+author.getAuthNumber();
         System.out.println("URL: "+url);
         testRestTemplate
-                .withBasicAuth(SECURITY_USER, SECURITY_USER_PASSWORD)
+                .withBasicAuth(ADMIN_ROLE, ADMIN_PASSWORD)
                 .delete(url);
     }
 }
